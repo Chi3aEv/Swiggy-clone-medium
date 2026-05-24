bfa3a2eee387b1d1b9ab647c2041f91fddd71a70
Bandya <chitamanisawant@gmail.com>
2026-05-24 10:48:28 +0530

    updated color in App.js

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ function App() {
   return (
     <div>
       <div style={{
-        background: '#070001',
+        background: '#080313',
         color: 'white',
         textAlign: 'center',
         padding: '8px',
         fontSize: '14px',
         fontWeight: 'bold'
       }}>
-        🚀 Version 4.0 — Deployed via Blue-Green DevSecOps Pipeline on AWS ECS
+        🚀 Version 1.0 — Deployed via Blue-Green DevSecOps Pipeline on AWS ECS
       </div>
       <Navigate/>
       <OffersBanner/>
